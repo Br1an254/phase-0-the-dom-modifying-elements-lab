e68410d768648680154bef1b16a551cb4f7f66ea
Brian Babu <briansmkenya@gmail.com>
2023-03-27 23:05:35 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById("main");
+
+ const newHeader = document.createElement('h1');
+ newHeader.id = "victory";
+ newHeader.innerHTML = "YOUR-NAME is the champion"
+ main.appendChild(newHeader)
+ 
+main.remove()
+
+
+// const newHeader =document.getElementById("victory");
+
